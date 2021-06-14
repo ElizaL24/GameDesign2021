dc8464267833d01afb67c7f5fe2fca1fa37fef84
Eliza <lamstere24@greenhill.org>
2021-06-14 08:29:12 -0500

    Added more

--- ListAndTuple.py
+++ ListAndTuple.py
@@ -2,6 +2,9 @@
 #6/10/2021
 # cntr / will put hashtag, make comment
 
+#Using a module/library by importing
+import random
+
 FruitList=["apples", "strawberries", "mangos", "pineapples"]
 
 #prints horizontally
@@ -26,6 +29,17 @@ for x in range(0,counter-1):  #prints all but the last one
 print(FruitList[counter-1])  #prints last one
 print()
 
+#Randomly picking a fruit
+#randint picks a random number, then finds the fruit that has that index
+indx=random.randint(0,counter-1)
+print("Your lucky fruit is", FruitList[indx])
+print()
+
+#choice randomly picks an iitem from list
+word=random.choice(FruitList)
+print("Your random fruit is", word)
+print()
+
 #checks if apples are in the list. If they are, it removes them
 if "apples" in FruitList:
     print("Yes, you got apples.")
@@ -46,3 +60,25 @@ print ()
 #Adds limes in last spot
 FruitList.append("limes")
 print(FruitList)
+print()
+print()
+print()
+
+#Making a tuple
+MoreFruit=("Apples", "Pears", "Bananas")
+
+
+#Converting the tuple to a list
+print("Tuple", MoreFruit)
+temp=list(MoreFruit)
+print("List", temp)
+
+#adding item to list/tuple
+temp.insert(1, "Kiwis")
+MoreFruit=tuple(temp)
+print("Modified Tuple", MoreFruit)
+print("Modified List", temp)
+
+for element in MoreFruit:
+    print(element)
+
