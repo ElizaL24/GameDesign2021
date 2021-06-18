97b871d1ad64268a21519f70bda8876d948e6907
Eliza <lamstere24@greenhill.org>
2021-06-18 09:39:25 -0500

    disaster has struck

--- mypygame.py
+++ mypygame.py
@@ -5,10 +5,16 @@ import pygame
 
 pygame.init()
 pygame.time.delay(100)
-WIDTH=600
-HEIGHT=400
+WIDTH=500
+HEIGHT=600
+bg=pygame.image.load(Desktop/Game Design 10/GameDesign2021/images/Sunsetave.jpg)
 #create object to open window
 surface=pygame.display.set_mode((WIDTH,HEIGHT))
+
+color=15, 15, 15,
+surface.fill(color)
+pygame.display.flip()
+
 
 color=(171,219,227)
 
@@ -27,6 +33,25 @@ pygame.draw.rect(surface, color, pygame.Rect(210, 30, 60, 60))
 pygame.display.flip()
 
 
+color=(157, 40, 72)
+
+
+pygame.draw.rect(surface, color, pygame.Rect(300, 30, 60, 60))
+pygame.display.flip()
+
+color=(140, 72, 87)
+
+
+pygame.draw.rect(surface, color, pygame.Rect(390, 30, 60, 60))
+pygame.display.flip()
+
+
+color=(184, 155, 136)
+
+
+pygame.draw.rect(surface, color, pygame.Rect(390, 30, 60, 60))
+pygame.display.flip()
+
 check=True
 while check:
     for event in pygame.event.get():
@@ -34,4 +59,3 @@ while check:
             check=False
 pygame.quit()
 
-pygame.draw.rect(surface, color, pygame.Rect(30, 30, 60, 60))
